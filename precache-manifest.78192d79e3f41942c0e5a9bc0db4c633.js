a545a845ef92a0ec4ec04015edc87fceeb3d5331
harvey-nuttall <harveyscottnuttall@gmail.com>
2020-03-26 14:06:46 +0000

    Updates

---
+++ precache-manifest.78192d79e3f41942c0e5a9bc0db4c633.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe67cbe01b1c9c1c43ba814daced074",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "3facf67e3f686aae0eb9",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eed7997836531ab5a6f7",
+    "url": "/myapp/static/js/2.9fca7837.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.9fca7837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3facf67e3f686aae0eb9",
+    "url": "/myapp/static/js/main.79ca6c4b.chunk.js"
+  },
+  {
+    "revision": "0ec61fdc48078999ef6c",
+    "url": "/myapp/static/js/runtime-main.d0f71498.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
